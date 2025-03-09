9a9a6e8ecde3dd1b341bf065698ce10bb7e7e6e2
Tilen Tinta <titi.tinta1@gmail.com>
2025-03-09 23:16:13 +0100

    PCB: added graphics, Firmware: USART IRQ working, pinout set

--- CH32V003F4P6/Peripheral/inc/ch32v00x_gpio.h
+++ CH32V003F4P6/Peripheral/inc/ch32v00x_gpio.h
@@ -32,14 +32,14 @@ typedef enum
 /* Configuration Mode enumeration */
 typedef enum
 {
-    GPIO_Mode_AIN = 0x0,
-    GPIO_Mode_IN_FLOATING = 0x04,
-    GPIO_Mode_IPD = 0x28,
-    GPIO_Mode_IPU = 0x48,
-    GPIO_Mode_Out_OD = 0x14,
-    GPIO_Mode_Out_PP = 0x10,
-    GPIO_Mode_AF_OD = 0x1C,
-    GPIO_Mode_AF_PP = 0x18
+    GPIO_Mode_AIN = 0x0,            // Analog IN
+    GPIO_Mode_IN_FLOATING = 0x04,   // IN floating
+    GPIO_Mode_IPD = 0x28,           // IN pull-down
+    GPIO_Mode_IPU = 0x48,           // IN pull-up
+    GPIO_Mode_Out_OD = 0x14,        // Out open-drain
+    GPIO_Mode_Out_PP = 0x10,        // Out push-pull
+    GPIO_Mode_AF_OD = 0x1C,         // Alternating function open-drain
+    GPIO_Mode_AF_PP = 0x18          // Alternating function push-pull
 } GPIOMode_TypeDef;
 
 /* GPIO Init structure definition */
