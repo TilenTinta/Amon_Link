982696a92fd0d0c4e32c67f2ca54df9e0b5e6843
TilenTinta <titi.tinta1@gmail.com>
2025-11-08 23:51:48 +0100

    Update
    bootloader progres, untested

--- Bootloader/CH32V003F4P6/User/main.c
+++ Bootloader/CH32V003F4P6/User/main.c
@@ -17,13 +17,20 @@ void USART1_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
 static void USART1_Init(void);
 static void LinkPinout_Init(void);
 static void UartSendBuffer(uint8_t* buffer, uint16_t length);
+static void UART_decode(uint8_t* raw_uart_data);
 static void UART_buffer_clear(void);
+static void UART_packet_parse(uint8_t* raw_data);
+static void UART_packetDataReset(void);
+static void flash_erase_app(void);
+static void flash_write_chunk(uint32_t address, uint8_t *data, uint16_t length);
+static uint16_t crc16_cal(const uint8_t *data, uint16_t length);
 
-void jump_to_app(void);
+static void jump_to_app(void);
 
 
 /* Structs */
 buffers_t buffers;
+packet_t packet;
 typedef void (*app_entry_t)(void);
 
 int main(void)
@@ -33,12 +40,27 @@ int main(void)
     LinkPinout_Init();
     USART1_Init();
 
-    // Check if button is pressed
+    buffers.flag_update_OK = 0;
+    buffers.flag_update_NOK = 0;
+
+    // Check if button is pressed, TODO: or reset flag set 
     if (GPIO_ReadInputDataBit(GPIOD, BTN_PIN) == Bit_SET) 
     {
-        // Turn on RED LED
-        GPIO_WriteBit(GPIOC, LED_RED, Bit_SET);
-        while(1){};
+        // Turn on BLUE LED
+        GPIO_WriteBit(GPIOD, LED_BLUE, Bit_SET);
+
+        while(1)
+        {
+            if (buffers.flag_USB_RX_end == 1) 
+            {
+                UART_decode(buffers.buffer_UART);               
+            }
+
+            if (buffers.flag_update_OK == 1) jump_to_app();     // Exit bootloader
+            if (buffers.flag_update_NOK == 1) GPIO_WriteBit(GPIOC, LED_RED, Bit_SET);   // Indicate an error
+
+
+        };
     }
     else
     {
@@ -150,8 +172,6 @@ static void USART1_Init(void)
  */
 static void UartSendBuffer(uint8_t* buffer, uint16_t length)
 {
-    DMA_Cmd(DMA1_Channel4, DISABLE);                                            // Disable DMA
-
     for(uint16_t cnt = 0; cnt < length; cnt++)
     {
         while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET)             // Waiting for sending finish
@@ -229,13 +249,280 @@ static void UART_buffer_clear(void)
 
 
 /*********************************************************************
+ * @fn      UART_packetDataReset
+ *
+ * @brief   Clear data in UART struct
+ *
+ * @return  none
+ */
+static void UART_packetDataReset(void)
+{
+    // Clear data struct
+    packet.sof = 0;
+    packet.plen = 0;
+    packet.addr = 0;
+    packet.cmd = 0;
+    for (int i = 0; i < packet.plen; i++)
+    {
+        packet.payload[i] = 0;
+    }
+    packet.crc16 = 0;
+}
+
+
+
+/*********************************************************************
+ * @fcn     UART_decode
+ *
+ * @brief   Initialises the GPIOs
+ *
+ * @return  none
+ */
+
+static void UART_decode(uint8_t* raw_uart_data)
+{
+
+    // Parse received buffer
+    UART_packetDataReset();
+    UART_packet_parse(raw_uart_data);
+
+    // CRC check
+    uint16_t cal_CRC = crc16_cal(&raw_uart_data[1], packet.plen - 2);
+
+    if (cal_CRC != packet.crc16)
+    {
+        // Send a response - error
+        buffers.buffer_UART[0] = SIG_SOF;
+        buffers.buffer_UART[1] = 5;
+        buffers.buffer_UART[2] = ID_PC;
+        buffers.buffer_UART[3] = CMD_ERR;
+        buffers.buffer_UART[4] = CODE_BAD_CRC;
+        buffers.buffer_UART[5] = crc16_cal(&buffers.buffer_UART[1], 4);
+
+        // Send
+        UartSendBuffer(buffers.buffer_UART,sizeof(buffers.buffer_UART));
+        return;
+    } 
+
+    // Clear uart buffer - used for transmit
+    UART_buffer_clear();
+   
+    // Check destination address
+    switch (packet.addr) 
+    {
+        // Destination device: PC - error
+        case ID_PC:
+            return;
+            break;
+
+        // Destination device: link
+        case ID_LINK:
+            
+            // Do action based on commands
+            switch (packet.cmd)
+            {
+                case CMD_GET_INFO:
+
+                    // Send a response 
+                    buffers.buffer_UART[0] = SIG_SOF;
+                    buffers.buffer_UART[1] = 5;
+                    buffers.buffer_UART[2] = ID_PC;
+                    buffers.buffer_UART[3] = CMD_ACK;
+                    buffers.buffer_UART[4] = CODE_SW_VER;
+                    buffers.buffer_UART[5] = SW_VER;
+                    buffers.buffer_UART[6] = crc16_cal(&buffers.buffer_UART[1], 5);
+
+                    // Send
+                    UartSendBuffer(buffers.buffer_UART,sizeof(buffers.buffer_UART));
+
+                    break;
+
+
+                case CMD_ERASE_APP:
+
+                    flash_erase_app();
+
+                    // Send a response 
+                    buffers.buffer_UART[0] = SIG_SOF;
+                    buffers.buffer_UART[1] = 5;
+                    buffers.buffer_UART[2] = ID_PC;
+                    buffers.buffer_UART[3] = CMD_ACK;
+                    buffers.buffer_UART[4] = crc16_cal(&buffers.buffer_UART[1], 3);
+
+                    // Send
+                    UartSendBuffer(buffers.buffer_UART,sizeof(buffers.buffer_UART));
+
+                    break;
+
+
+                case CMD_WRITE:
+                    {
+                        uint32_t addr = packet.payload[0]        |
+                                   (packet.payload[1] << 8)  |
+                                   (packet.payload[2] << 16) |
+                                   (packet.payload[3] << 24);
+
+                        flash_write_chunk(addr, &packet.payload[4], sizeof(packet.payload) - 4);
+
+                        // Send a response 
+                        buffers.buffer_UART[0] = SIG_SOF;
+                        buffers.buffer_UART[1] = 5;
+                        buffers.buffer_UART[2] = ID_PC;
+                        buffers.buffer_UART[3] = CMD_ACK;
+                        buffers.buffer_UART[4] = CODE_DATA_WRITEN;
+                        buffers.buffer_UART[5] = crc16_cal(&buffers.buffer_UART[1], 4);
+
+                        // Send
+                        UartSendBuffer(buffers.buffer_UART,sizeof(buffers.buffer_UART));
+                        break;
+                    }
+                    
+
+                case CMD_JUMP_APP:
+
+                    // Send a respnse 
+                    buffers.buffer_UART[0] = SIG_SOF;
+                    buffers.buffer_UART[1] = 5;
+                    buffers.buffer_UART[2] = ID_PC;
+                    buffers.buffer_UART[3] = CMD_ACK;
+                    buffers.buffer_UART[4] = CODE_EXIT_BOOT;
+                    buffers.buffer_UART[5] = crc16_cal(&buffers.buffer_UART[1], 4);
+
+                    // Send
+                    UartSendBuffer(buffers.buffer_UART,sizeof(buffers.buffer_UART));
+
+                    jump_to_app();
+
+                    break;
+            }
+
+            break;
+
+        default:
+            // Unavailable
+            break;
+    
+    }
+}
+
+
+
+ /*********************************************************************
+ * @fcn     UART_packet_parse
+ *
+ * @brief   Spliting packet in to information parts
+ *
+ * @return  none
+ */
+
+static void UART_packet_parse(uint8_t* raw_data)
+{
+    packet.sof = *raw_data;
+    packet.plen = *(raw_data + 1);
+    packet.addr = *(raw_data + 2);
+    packet.cmd = *(raw_data + 3);
+    for (int i = 0; i < packet.plen; i++)
+    {
+        packet.payload[i] = *(raw_data + HEADER_SHIFT + i);
+    }
+    packet.crc16 = *(raw_data + (packet.plen + 1));
+}
+
+
+
+/*********************************************************************
+ * @fcn     erase_app_space
+ *
+ * @brief   Erase part of flash where app is places
+ *
+ * @return  none
+ */
+static void flash_erase_app(void)
+{
+    FLASH_Unlock();
+
+    for(uint32_t addr = APP_START_ADDRESS; addr < APP_END_ADDRESS; addr += PAGE_SIZE)
+    {
+        FLASH_ErasePage(addr);
+        while(FLASH_GetStatus() == FLASH_BUSY); // wait 
+    }
+
+    FLASH_Lock();
+}
+
+
+
+/*********************************************************************
+ * @fcn     flash_write_chunk
+ *
+ * @brief   Write single page to flash
+ *
+ * @return  none
+ */
+static void flash_write_chunk(uint32_t address, uint8_t *data, uint16_t length)
+{
+    FLASH_Unlock();
+
+    for(uint16_t i = 0; i < length; i += 4)
+    {
+        uint32_t word =  data[i] |
+                        (data[i+1] << 8) |
+                        (data[i+2] << 16) |
+                        (data[i+3] << 24);
+
+        FLASH_ProgramWord(address + i, word);
+        while(FLASH_GetStatus() == FLASH_BUSY);
+    }
+
+    FLASH_Lock();
+}
+
+
+
+/*********************************************************************
+ * @fcn     crc16_cal
+ *
+ * @brief   Calculates 2 byte crc over data
+ *          Modbus RTU-style CRC-16 algorithm
+ *          https://ctlsys.com/support/how_to_compute_the_modbus_rtu_message_crc/
+ *
+ * @return  crc value
+ */
+static uint16_t crc16_cal(const uint8_t *data, uint16_t length)
+{
+    uint16_t crc = 0xFFFF;                  // don¡¯t start with zero, this would make some leading zeros in data invisible
+
+    for (uint16_t i = 0; i < length; i++)
+    {
+        crc ^= data[i];                     // XOR byte into least sig. byte of crc
+
+        for (uint8_t j = 8; j != 0; j--)    // Loop over each bit
+        {
+            if ((crc & 0x0001) != 0)        // If the LSB is set
+            {
+                crc >>= 1;                  // Shift right and XOR 0xA001
+                crc ^= 0xA001;
+            }       
+            else                            // Else LSB is not set
+            {
+                crc >>= 1;                  // Just shift right
+            }                            
+        }
+    }
+    // Note, this number has low and high bytes swapped, so use it accordingly
+    return crc;
+}
+
+
+
+/*********************************************************************
  * @fn      jump_to_app
  *
  * @brief   Jump from bootloader to main program
  *
  * @return  none
  */
-void jump_to_app(void)
+static void jump_to_app(void)
 {
     // Get app vector table
     uint32_t *app_vector_table = (uint32_t *)APP_START_ADDRESS;
@@ -267,19 +554,6 @@ void jump_to_app(void)
     while (1) { }
 }
 
-// void jump_to_app(void)
-// {
-//     __disable_irq(); // disable interrupts
-//     NVIC_ClearPendingIRQ(USART_IT_RXNE);
 
-//     USART_DeInit(USART1);
-//     GPIO_DeInit(GPIOD);
-//     GPIO_DeInit(GPIOC);
 
-//     // Jump
-//     app_entry_t app_reset = (app_entry_t)(APP_START_ADDRESS);
-//     app_reset();
 
-//     // optional infinite loop
-//     while (1) { }
-// }
--- Bootloader/CH32V003F4P6/User/main.h
+++ Bootloader/CH32V003F4P6/User/main.h
@@ -11,14 +11,18 @@
 #define BOOTLOADER_H
 
 #include "ch32v00x.h" 
+#include "ch32v00x_flash.h"
 
 
 /*###########################################################################################################################################################*/
 /* Defines */
 
 // Device constants
-#define BOOT_DATE        "October 2025" // Date of software
-#define APP_START_ADDRESS  ((uint32_t)0x08001000u)
+#define BOOT_DATE        "October 2025"             // Date of software
+#define SW_VER              01                      // Version of software
+#define APP_START_ADDRESS  ((uint32_t)0x08001000u)  // Address of start of main program
+#define APP_END_ADDRESS     0x08003FFF              // End address of flash
+#define PAGE_SIZE           64                      // Page size in bytes
 
 // Pinout
 #define LED_RED             GPIO_Pin_0  // PC0: Pin of red LED
@@ -28,11 +32,7 @@
 #define USART_RX            GPIO_Pin_6  // PD6: USART RX pin (mistake - pin remaping)
 #define USART_TX            GPIO_Pin_5  // PD5: USART TX pin (mistake - pin remaping)
 
-
-// Driver defines
-#define DRIVER_VER              0x01    // Version of current driver
-
-// Entities / IDs (1 byte)
+// IDs (1 byte)
 #define ID_PC                   0x01    // Address: PC
 #define ID_LINK                 0x10    // Address: Link board
 
@@ -40,13 +40,20 @@
 #define HEADER_SHIFT            0x08    // Number of bytes before payload
 #define SIG_SOF                 0xAA    // Start-Of-Frame
 
-// Flags (1 byte) (bit on this place is set or not)
-#define FLAG_ACK                0x01    // frame is a reply
-#define FLAG_ERR                0x02    // processing error; payload carries error code
-#define FLAG_STREAM             0x03    // telemetry stream frame
-#define FLAG_FRAG               0x04    // fragmented payload; optional future use
-#define FLAG_NaN                0xF0    // bit4..7: reserved
+// Commands (1 byte)
+#define CMD_GET_INFO            0x01    // Bootloader version
+#define CMD_ERASE_APP           0x02    // Erase application area (0x08001000 .. end)
+#define CMD_WRITE               0x03    // Write a chunk: [addr(4B) + data...]
+#define CMD_VERIFY              0x04    // Optional: verify CRC of whole app
+#define CMD_JUMP_APP            0x05    // Jump to application
+#define CMD_ACK                 0x80    // Replies
+#define CMD_ERR                 0x81    // Error reply with error code
 
+// Codes (1 byte)
+#define CODE_BAD_CRC            0x01    // calculated CRC and received CRC doesnt match
+#define CODE_SW_VER             0x02    // Return software version
+#define CODE_EXIT_BOOT          0x03    // Exiting bootloader, jumping to application
+#define CODE_DATA_WRITEN        0x10    // Succesfuly writen data packet
 
 
 /*###########################################################################################################################################################*/
@@ -54,22 +61,28 @@
 typedef struct {
 
     // PC -> Drone and Drone -> PC //
-    uint8_t     buffer_UART[64];        // Buffer for saving USB data
+    uint8_t     buffer_UART[128];       // Buffer for saving USB data
     uint8_t     flag_new_uart_rx_data;  // Flag indicating a new data has arrived (packet is not complete)
     uint8_t     flag_new_uart_tx_data;  // Flag indicating a new data is ready to send
     uint8_t     flag_USB_RX_end;        // Flag for new complete USB command (PC -> link) - start decode
+
+    uint8_t     flag_update_OK;         // Flag for indicating end of sucessful update
+    uint8_t     flag_update_NOK;        // Flag for indicating end of unsucessful update
 
 } buffers_t;
 
 
 typedef struct {
 
-    uint8_t SOF;        
-    uint8_t CMD;
-    uint16_t LEN;
-    uint8_t PAYLOAD[256];
-    uint16_t CRC16;
+    uint8_t sof;                        // Start of frame
+    uint16_t plen;                      // Packet lenght
+    uint8_t addr;                       // Address of targeted device
+    uint8_t cmd;                        // Command
+    uint8_t payload[128];               // Payload data
+    uint16_t crc16;                     // CRC calculated over: plen + addr + cmd + payload
 
 } packet_t;
+
+
 
 #endif
